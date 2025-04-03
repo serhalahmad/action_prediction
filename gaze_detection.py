b0d046caa5759cada09160625bd0f266cbbc6023
Ahmad Serhal <ahmadserhal.289t@hotmail.com>
2025-04-03 21:24:41 +0100

    Implemented face detection

--- gaze_detection.py
+++ gaze_detection.py
@@ -1,12 +1,22 @@
 import cv2
+import dlib
 
 cap = cv2.VideoCapture(0)
 
+detector = dlib.get_frontal_face_detector()
+
 while True:
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = detector(gray)
+    for face in faces:
+        x, y = face.left(), face.top()
+        x1, y1 = face.right(), face.bottom()
+        cv2.rectangle(frame, (x, y), (x1, y1), (0, 255,0), 2)
 
     cv2.imshow('Camera Feed', frame)
-    
+
     if cv2.waitKey(1) == ord('q'):
         break
 
