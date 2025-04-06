afc74a2536a512c17aa33503f980cffedad1e5ae
Ahmad Serhal <ahmadserhal.289t@hotmail.com>
2025-04-06 16:19:16 +0100

    Detected blinking of each eye

--- gaze_detection.py
+++ gaze_detection.py
@@ -1,9 +1,29 @@
 import cv2
 import dlib
+from math import hypot
 
 cap = cv2.VideoCapture(0)
 
 detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+def midpoint(p1, p2):
+    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+def get_blinking_ratio(eye_points, facial_landmarks):
+    left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
+    right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
+    top_point = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
+    bottom_point = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
+    hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
+    ver_line = cv2.line(frame, top_point, bottom_point, (0, 255, 0), 2)
+
+    hor_line_length = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+    ver_line_length = hypot((top_point[0] - bottom_point[0]), (top_point[1] - bottom_point[1]))
+    
+    return hor_line_length / ver_line_length
 
 while True:
     ret, frame = cap.read()
@@ -11,9 +31,14 @@ while True:
 
     faces = detector(gray)
     for face in faces:
-        x, y = face.left(), face.top()
-        x1, y1 = face.right(), face.bottom()
-        cv2.rectangle(frame, (x, y), (x1, y1), (0, 255,0), 2)
+        landmarks = predictor(gray, face)
+        
+        right_eye_ratio = get_blinking_ratio([36, 37, 38, 39, 40, 41], landmarks)
+        left_eye_ratio = get_blinking_ratio([42, 43, 44, 45, 46, 47], landmarks)
+        if left_eye_ratio > 5:
+            cv2.putText(frame, "LEFT", (50, 150), font, 3, (255, 0, 0))
+        if right_eye_ratio > 5:
+            cv2.putText(frame, "RIGHT", (100, 300), font, 3, (255, 0, 0))
 
     cv2.imshow('Camera Feed', frame)
 
