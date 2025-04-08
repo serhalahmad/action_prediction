10f53fa0dc5618f344dd70ed286e7d5ca1510b12
Ahmad Serhal <ahmadserhal.289t@hotmail.com>
2025-04-08 18:41:25 +0100

    Flipped the image and updated the colors of the landmarks and connections

--- hand_shape_detection.py
+++ hand_shape_detection.py
@@ -21,17 +21,21 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5, m
         ret, frame = cap.read()
 
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = cv2.flip(image, 1) # Flip on horizontal
         
         image.flags.writeable = False
         
         results = hands.process(image) # Detections
         image.flags.writeable = True
-        image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         print(results.multi_hand_landmarks)
         # Rendering results
         if results.multi_hand_landmarks:
             for num, hand in enumerate(results.multi_hand_landmarks):
-                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS)
+                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
+                                          mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                          mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2),
+                                          )
 
         cv2.imshow("Hand Tracking", image)
 
