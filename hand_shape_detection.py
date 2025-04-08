b7fd04d3bba38bf91ae9d0f5c6439470c127c001
Ahmad Serhal <ahmadserhal.289t@hotmail.com>
2025-04-08 18:28:32 +0100

    Implemented hand detection and got the needed landmarks

---
+++ hand_shape_detection.py
@@ -0,0 +1,42 @@
+# Script to detect the hand grip shape and location
+
+import mediapipe as mp
+import cv2
+import numpy as np
+import uuid
+import os
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+# This code is to detect objects using their color
+
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        image.flags.writeable = False
+        
+        results = hands.process(image) # Detections
+        image.flags.writeable = True
+        image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        print(results.multi_hand_landmarks)
+        # Rendering results
+        if results.multi_hand_landmarks:
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS)
+
+        cv2.imshow("Hand Tracking", image)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
